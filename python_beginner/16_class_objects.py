0144a6d68e4b2632c486ecca9ae1d70fdbc38d2c
mridul162 <mridulbd75@gmail.com>
2025-11-15 21:36:42 +0600

    add comment to 16_class_objects.py

--- python_beginner/16_class_objects.py
+++ python_beginner/16_class_objects.py
@@ -1,15 +1,17 @@
+# Defining a class Human with attributes and methods
+
 class Human:
-    def __init__(self, n, o):
+    def __init__(self, n, o):       # Constructor to initialize name and occupation
         self.name = n
         self.occupation = o
 
-    def do_work(self):
+    def do_work(self):              # Method to perform work based on occupation
         if self.occupation == "tennis player":
             print(self.name,"plays tennis")
         elif self.occupation == "actor":
             print(self.name,"shoots a film")
 
-    def speaks(self):
+    def speaks(self):   
         print(self.name,"says how are you?")
 
 tom = Human("tom cruise", "actor")
