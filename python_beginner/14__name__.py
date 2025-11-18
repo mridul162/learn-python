3134f53549de231383379aadc63f178c0b55f596
mridul162 <mridulbd75@gmail.com>
2025-11-18 22:42:22 +0600

    modify 14__name__.py

--- python_beginner/14__name__.py
+++ python_beginner/14__name__.py
@@ -1,10 +1,14 @@
 # Demonstration of __name__ variable:
+import modules.area as a
 
 def calculate_area(base,height): 
-    print("__name__: ", __name__)
-    return 1/2*(base*height)
+    print("__name__: ", __name__)            # Print the __name__ variable
+    return 1/2 * (base*height)
 
-if __name__ == "__main__":  #
-    print("I am in area.py")
-    a = calculate_area(10, 20) 
-    print("Area: ", a) 
+if __name__ == "__main__":                   # This block will execute only if this script is run directly
+    print("I am in __name__.py")
+    area1 = calculate_area(10, 20) 
+    print("Area from __name__.py: ", area1)
+    
+    area2 = a.calculate_area(10, 20)         # Importing and using function from another module
+    print("Area from imported module: ", area2)
