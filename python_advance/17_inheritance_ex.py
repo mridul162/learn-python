8a555f5c09593adfa7be37ae1b098a4d8c201d92
mridul162 <mridul162@gmail.com>
2025-11-20 21:54:50 +0600

    add comment explaining inheritance example

--- python_advance/17_inheritance_ex.py
+++ python_advance/17_inheritance_ex.py
@@ -1,18 +1,19 @@
+# Inheritance Example
 class Animal:
-    def __init__(self, habitat):
-        self.habitat = habitat
+    def __init__(self, habitat):  # Constructor with habitat parameter
+        self.habitat = habitat    # Initialize habitat attribute
 
-    def print_habitat(self):
+    def print_habitat(self):      # Method to print habitat
         print(self.habitat)
 
     def sound(self):
         print("Some Animal Sound")
 
-class Dog(Animal):
+class Dog(Animal):                # Dog class inherits from Animal
     def __init__(self):
         super().__init__("Kennel")
 
-    def sound(self):
+    def sound(self):              # Overriding the sound method
         print("Woof woof!")
 
 x = Dog()
